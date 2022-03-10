21b6e0031d0b4938f42f41ae466505bd8a2e464d
Nilton Rocha <57201440+niltonrochadeveloper@users.noreply.github.com>
2022-03-09 23:38:44 -0300

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+module.exports = {
+  "development": {
+    "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "dialect": "mysql"
+  }
+}
+
