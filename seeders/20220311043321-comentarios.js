a5f9a446e91e65df4cde0b5f3b3e90226833ce6f
Nilton Rocha <jornalista.nilton@gmail.com>
2022-03-16 01:45:29 -0300

    multiple eager loading finish + modals(view, create, update, insert)

--- seeders/20220311043321-comentarios.js
+++ seeders/20220311043321-comentarios.js
@@ -35,6 +35,46 @@ module.exports = {
       tarefaId: '1',
       createdAt: new Date(),
       updatedAt: new Date()
+    }, {
+      conteudo: 'primeiro comentário',
+      link: '/:tarefaId/:usuarioId/:id',
+      arquivoId: '5',
+      usuarioId: '2',
+      tarefaId: '5',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      conteudo: 'primeiro comentário',
+      link: '/:tarefaId/:usuarioId/:id',
+      arquivoId: '5',
+      usuarioId: '2',
+      tarefaId: '5',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      conteudo: 'primeiro comentário',
+      link: '/:tarefaId/:usuarioId/:id',
+      arquivoId: '5',
+      usuarioId: '2',
+      tarefaId: '5',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      conteudo: 'primeiro comentário',
+      link: '/:tarefaId/:usuarioId/:id',
+      arquivoId: '5',
+      usuarioId: '2',
+      tarefaId: '4',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      conteudo: 'primeiro comentário',
+      link: '/:tarefaId/:usuarioId/:id',
+      arquivoId: '5',
+      usuarioId: '2',
+      tarefaId: '3',
+      createdAt: new Date(),
+      updatedAt: new Date()
     }], {});
     
   },
